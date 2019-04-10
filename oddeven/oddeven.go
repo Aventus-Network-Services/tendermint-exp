b1beb2c2dd1f92d8e9031b565f6b63ba214dda64
Angus Hamill <angus.hamill@artos.io>
2019-04-10 12:20:57 +0100

    Implement nonce

--- oddeven/oddeven.go
+++ oddeven/oddeven.go
@@ -1,9 +1,11 @@
 package oddeven
 
 import (
+	"bytes"
 	"encoding/binary"
 	"fmt"
 	"github.com/tendermint/tendermint/abci/types"
+	"strconv"
 )
 
 const (
@@ -13,8 +15,9 @@ const (
 )
 
 type state struct {
-	size    uint64
-	height  uint64
+	size    int64
+	height  int64
+	appHash []byte
 }
 
 type OddEvenApplication struct {
@@ -23,19 +26,19 @@ type OddEvenApplication struct {
 }
 
 func (app *OddEvenApplication) Info(req types.RequestInfo) types.ResponseInfo {
-	return types.ResponseInfo{Data: fmt.Sprintf("{\"hashes\":%v,\"txs\":%v}", app.state.height, app.state.size)}
+	return types.ResponseInfo{
+		Data: fmt.Sprintf("{\"hashes\":%v,\"txs\":%v}", app.state.height, app.state.size),
+		LastBlockHeight: app.state.height,
+		LastBlockAppHash: app.state.appHash}
 }
 
 func (app *OddEvenApplication) DeliverTx(tx []byte) types.ResponseDeliverTx {
-	if len(tx) > 8 {
+	txValue, err := parseValue(tx)
+	if err != nil {
 		return types.ResponseDeliverTx{
 			Code: codeTypeEncodingError,
-			Log:  fmt.Sprintf("Max tx size is 8 bytes, got %d", len(tx))}
+			Log: fmt.Sprintf("%v", err)}
 	}
-
-	tx8 := make([]byte, 8)
-	copy(tx8[len(tx8)-len(tx):], tx)
-	txValue := binary.BigEndian.Uint64(tx8)
 
 	if txValue % 2 == 1 {
 		return types.ResponseDeliverTx{
@@ -48,15 +51,12 @@ func (app *OddEvenApplication) DeliverTx(tx []byte) types.ResponseDeliverTx {
 }
 
 func (app *OddEvenApplication) CheckTx(tx []byte) types.ResponseCheckTx {
-	if len(tx) > 8 {
+	txValue, err := parseValue(tx)
+	if err != nil {
 		return types.ResponseCheckTx{
 			Code: codeTypeEncodingError,
-			Log:  fmt.Sprintf("Max tx size is 8 bytes, got %d", len(tx))}
+			Log: fmt.Sprintf("%v", err)}
 	}
-
-	tx8 := make([]byte, 8)
-	copy(tx8[len(tx8)-len(tx):], tx)
-	txValue := binary.BigEndian.Uint64(tx8)
 
 	if txValue % 2 == 1 {
 		return types.ResponseCheckTx{
@@ -69,11 +69,18 @@ func (app *OddEvenApplication) CheckTx(tx []byte) types.ResponseCheckTx {
 
 func (app *OddEvenApplication) Commit() (resp types.ResponseCommit) {
 	appHash := make([]byte, 8)
-	binary.PutUvarint(appHash, app.state.size)
+	binary.PutVarint(appHash, app.state.size)
 	app.state.height++
+	app.state.appHash = appHash
+
 	return types.ResponseCommit{Data: appHash}
 }
 
 func (app *OddEvenApplication) Query(reqQuery types.RequestQuery) types.ResponseQuery {
 	return types.ResponseQuery{Value: []byte(fmt.Sprintf("%v", app.state.size))}
+}
+
+func parseValue(tx []byte) (int, error) {
+	txValues := bytes.Split(tx, []byte(":"))
+	return strconv.Atoi(string(txValues[0]))
 }
